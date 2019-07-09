9122e50546b23713729b594c877dc9bea81766de
andreisha <andreamourelo@MacBook-Pro-de-Andrea.local>
2019-07-09 17:49:21 -0300

    senao / enquanto / if + output no final

--- trabmain.tab.h
+++ trabmain.tab.h
@@ -1,13 +1,14 @@
-/* A Bison parser, made by GNU Bison 3.0.5.  */
+/* A Bison parser, made by GNU Bison 2.3.  */
 
-/* Bison interface for Yacc-like parsers in C
+/* Skeleton interface for Bison's Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015, 2018 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
 
-   This program is free software: you can redistribute it and/or modify
+   This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,7 +16,9 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -30,73 +33,89 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-#ifndef YY_YY_TRABMAIN_TAB_H_INCLUDED
-# define YY_YY_TRABMAIN_TAB_H_INCLUDED
-/* Debug traces.  */
-#ifndef YYDEBUG
-# define YYDEBUG 0
-#endif
-#if YYDEBUG
-extern int yydebug;
-#endif
-
-/* Token type.  */
+/* Tokens.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-  enum yytokentype
-  {
-    ENTRADA = 258,
-    SAIDA = 259,
-    FIM = 260,
-    FACA = 261,
-    VEZES = 262,
-    ENQUANTO = 263,
-    SE = 264,
-    ENTAO = 265,
-    SENAO = 266,
-    INC = 267,
-    ZERA = 268,
-    NAME = 269,
-    ESPACO = 270,
-    FIMFACA = 271,
-    FIMENTAO = 272,
-    FIMSENAO = 273,
-    FIMENQUANTO = 274,
-    FIMSE = 275,
-    ROWEND = 276,
-    NUMBER = 277,
-    ASSIGN = 278,
-    PLUS = 279,
-    AP = 280,
-    FP = 281,
-    MULT = 282,
-    END = 283,
-    PROGRAM = 284,
-    id = 285
-  };
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     ENTRADA = 258,
+     SAIDA = 259,
+     FIM = 260,
+     FACA = 261,
+     VEZES = 262,
+     ENQUANTO = 263,
+     SE = 264,
+     ENTAO = 265,
+     SENAO = 266,
+     INC = 267,
+     ZERA = 268,
+     NAME = 269,
+     ESPACO = 270,
+     FIMFACA = 271,
+     FIMENTAO = 272,
+     FIMSENAO = 273,
+     FIMENQUANTO = 274,
+     FIMSE = 275,
+     ROWEND = 276,
+     NUMBER = 277,
+     ASSIGN = 278,
+     PLUS = 279,
+     AP = 280,
+     FP = 281,
+     MULT = 282,
+     END = 283,
+     PROGRAM = 284,
+     id = 285
+   };
 #endif
+/* Tokens.  */
+#define ENTRADA 258
+#define SAIDA 259
+#define FIM 260
+#define FACA 261
+#define VEZES 262
+#define ENQUANTO 263
+#define SE 264
+#define ENTAO 265
+#define SENAO 266
+#define INC 267
+#define ZERA 268
+#define NAME 269
+#define ESPACO 270
+#define FIMFACA 271
+#define FIMENTAO 272
+#define FIMSENAO 273
+#define FIMENQUANTO 274
+#define FIMSE 275
+#define ROWEND 276
+#define NUMBER 277
+#define ASSIGN 278
+#define PLUS 279
+#define AP 280
+#define FP 281
+#define MULT 282
+#define END 283
+#define PROGRAM 284
+#define id 285
 
-/* Value type.  */
+
+
+
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-
-union YYSTYPE
+typedef union YYSTYPE
+#line 15 "trabmain.y"
 {
-#line 14 "trabmain.y" /* yacc.c:1910  */
-
    char *str;
    int  number;
-
-#line 90 "trabmain.tab.h" /* yacc.c:1910  */
-};
-
-typedef union YYSTYPE YYSTYPE;
-# define YYSTYPE_IS_TRIVIAL 1
+}
+/* Line 1529 of yacc.c.  */
+#line 114 "trabmain.tab.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
 #endif
-
 
 extern YYSTYPE yylval;
 
-int yyparse (void);
-
-#endif /* !YY_YY_TRABMAIN_TAB_H_INCLUDED  */
--- trabmain.y
+++ trabmain.y
@@ -5,6 +5,7 @@
 
   	FILE * fp;
         int yylex();
+        char * output;
         void yyerror(const char *s){
                 fprintf(stderr, "%s\n", s);
         };
@@ -31,26 +32,28 @@ lines: line {$$ = $1;}
 
 line: statement ROWEND {$$ = $1;};
 
-statement: ENTRADA varlist{fp = fopen("provolone.txt","w");fprintf (fp,"\ninput(%s);\n", $2);};
-        | SAIDA varlist {printf ("\noutput(%s);", $2);};
-        | cmds ;
-        | FIM {fprintf (fp,"\nend of program;\n");fclose(fp); exit(1);};
-        | FIMFACA {fprintf (fp,"\nfim do faca;\n"); $$ = $1;};
-
+statement: ENTRADA varlist{fp = fopen("provolone.txt","w");fprintf (fp,"\ninput(%s);\n", $2);}
+        | SAIDA varlist {output = (char *)malloc(11+(strlen($2))*sizeof(char)); 
+			strcpy(output,"\noutput(");
+			strcat(output,$2);
+			strcat(output,");\n");}
+        | cmds 
+        | FIM {	fprintf(fp,output);
+		fprintf (fp,"\nend of program;\n");fclose(fp); exit(1);}
+        
 varlist: id varlist {char * result = malloc(strlen($1) + strlen($2) + 1); strcpy(result, $1); strcat(result, ","); strcat(result, $2); $$=result;}
        | id {$$ = $1;};
        
 cmds: cmd cmds {char * result = malloc(strlen($1) + strlen($2) + 1); strcpy(result, $1); strcat(result, ";\n"); strcat(result, $2); $$=result;}
-
-        | ROWEND cmd {$$=$2;}
+        | cmd {$$=$1;}
     
-cmd: FACA id VEZES {fprintf(fp,"\ndurante %s vezes faca {\n\n", $2);} cmds {fprintf(fp,"\n}\n"); $$ = $1;}
-	| ENQUANTO id FACA cmds FIMENQUANTO {}
-	| SE id ENTAO cmds FIMENTAO SENAO cmds FIMSENAO{}
-	| SE id ENTAO cmds FIMSE{}
-        | id ASSIGN id {fprintf(fp,"copia(%s,%s);\n",$3,$1);}
-	| INC AP id FP {fprintf (fp,"incrementa(%s);\n", $3); $$=$3;}
-	| ZERA AP id FP {fprintf (fp,"zera(%s);",$3);};
+cmd: FACA id VEZES {fprintf(fp,"\ndurante %s vezes faca {\n\n", $2);} ROWEND cmds FIMFACA {fprintf(fp,"\n}\n");fprintf (fp,"\nfim do faca;\n"); $$ = $1;}
+	| ENQUANTO id FACA {fprintf(fp,"\nenquanto %s > 0 faca {\n\n", $2);} ROWEND cmds FIMENQUANTO {fprintf(fp,"\n}\n");fprintf (fp,"\nfim do enquanto;\n"); $$ = $1;}
+	| SE id ENTAO {fprintf(fp,"\nse %s > 0 entao {\n\n", $2);} ROWEND cmds FIMENTAO ROWEND SENAO {fprintf(fp,"\n}\n"); fprintf(fp,"\nfim do entao;\n"); fprintf(fp,"senao {\n\n");} ROWEND cmds FIMSENAO {fprintf(fp,"\n}\n");fprintf (fp,"\nfim do senao;\n"); $$ = $1;}
+	| SE id ENTAO {fprintf(fp,"\nse %s > 0 entao {\n\n", $2);} ROWEND cmds FIMSE {fprintf(fp,"\n}\n"); fprintf(fp,"\nfim do se\n");}
+        | id ASSIGN id ROWEND {fprintf(fp,"copia(%s,%s);\n",$3,$1);}
+	| INC AP id FP ROWEND {fprintf (fp,"incrementa(%s);\n", $3); $$=$3;}
+	| ZERA AP id FP ROWEND {fprintf (fp,"zera(%s);\n",$3);};
 
 %%
 
